c5562fdc35ff6e4d011a3240e0e506f8c896e41c
kitsunyan <kitsunyan@users.noreply.github.com>
2017-01-26 03:45:22 +0300

    Allow to reply to any private/group chats

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Little Chen Bot
 
-Telegram bot which allows you to fetch images from iqdb.
+Telegram bot that allows you to fetch images from iqdb.
 
-## Building and Installing
+## Dependencies
+
+`libwebp` — Allows to convert WebP images to PNG.  
+`tor` — Bot uses Tor to access *booru servers.
+
+## Building and Running
 
 Run `sbt run` to run bot and `sbt assembly` to build a standalone jar file.
 
@@ -10,23 +15,22 @@ Run `sbt run` to run bot and `sbt assembly` to build a standalone jar file.
 
 You should create `littlechenbot.conf` in the project directory.
 
-You should specify `bot.token` key with Telegram API token.
+Available properties:
 
-You should also specify a list allowed chat IDs using `bot.chats` key. Bot will reply only to these chats.
+* `bot.token` — Telegram API token (String, required)
+* `bot.chats` — List of allowed chatIDs (list of Long values, optional)
+* `bot.chatsAnyPrivate` — Allows to reply to all private chats (Boolean, optional)
+* `bot.chatsAnyGroup` — Allows to reply to all group chats (Boolean, optional)
 
 Example configuration:
 
 ```properties
 bot {
-	token = "385946732:Jsi19If0si1_dj8AojOS9N5dhsJc8jJ7S98"
-	chats = [
-		83420754235, // Chat 1
-		-4759243795 // Chat 2
-	]
+    token = "385946732:Jsi19If0si1_dj8AojOS9N5dhsJc8jJ7S98"
+    chatsAnyPrivate = true
+    chats = [
+        83420754235, // Chat 1
+        -4759243795 // Chat 2
+    ]
 }
 ```
-
-## Dependencies
-
-`libwebp` — Allows to convert WebP images to PNG.  
-`tor` — Bot uses Tor to access *booru servers.
--- src/main/scala/nya/kitsunyan/littlechenbot/BotApplication.scala
+++ src/main/scala/nya/kitsunyan/littlechenbot/BotApplication.scala
@@ -1,6 +1,6 @@
 package nya.kitsunyan.littlechenbot
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config._
 
 import info.mukel.telegrambot4s.api._
 import info.mukel.telegrambot4s.methods._
@@ -14,12 +14,28 @@ import scalaj.http._
 object BotApplication extends App {
   private val config = ConfigFactory.parseFile(new java.io.File("littlechenbot.conf"))
 
+  private def config[T](getter: String => T, key: String, fallback: T): T = {
+    try {
+      getter(key)
+    } catch {
+      case _: ConfigException => fallback
+    }
+  }
+
   object ShikigamiBot extends TelegramBot with Polling with IqdbCommand {
     override def token: String = config.getString("bot.token")
     override val botNickname: Future[String] = request(GetMe).map(_.username.getOrElse(""))
 
-    private val chats = config.getLongList("bot.chats")
+    private val chats = config(config.getLongList, "bot.chats", java.util.Collections.emptyList)
+    private val chatsAnyPrivate = config(config.getBoolean, "bot.chatsAnyPrivate", false)
+    private val chatsAnyGroup = config(config.getBoolean, "bot.chatsAnyGroup", false)
     private val startTime = System.currentTimeMillis / 1000
+
+    override def filterChat(message: Message): Boolean = {
+      message.date >= startTime && (chats.contains(message.chat.id) ||
+        chatsAnyPrivate && Set("private").contains(message.chat.`type`) ||
+        chatsAnyGroup && Set("group", "supergroup").contains(message.chat.`type`))
+    }
 
     override def http(url: String, proxy: Boolean): HttpRequest = {
       Some(Http(url).timeout(connTimeoutMs = 10000, readTimeoutMs = 10000)
@@ -27,8 +43,6 @@ object BotApplication extends App {
         if (proxy) request.proxy("localhost", 9050, java.net.Proxy.Type.SOCKS) else request
       }.get
     }
-
-    override def filterChat(message: Message): Boolean = message.date >= startTime && chats.contains(message.chat.id)
   }
 
   ShikigamiBot.run()
