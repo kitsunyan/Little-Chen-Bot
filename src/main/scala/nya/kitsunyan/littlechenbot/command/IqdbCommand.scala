dacbef8f151be54f4ca5b2c7af35f20c87d1457e
kitsunyan <kitsunyan@users.noreply.github.com>
2017-03-24 02:21:18 +0300

    Handle booru pages with images together
    Switch to next result on failure.
    Refactor the source code.

--- src/main/scala/nya/kitsunyan/littlechenbot/command/IqdbCommand.scala
+++ src/main/scala/nya/kitsunyan/littlechenbot/command/IqdbCommand.scala
@@ -15,7 +15,7 @@ trait IqdbCommand extends Command with ExtractImage with Http {
   }
 
   private def handleMessageInternal(arguments: Arguments)(implicit message: Message): Unit = {
-    case class IqdbResult(url: String, similarity: Int, matches: Boolean)
+    case class IqdbResult(url: String, booruService: BooruService, similarity: Int, matches: Boolean)
 
     def sendIqdbRequest(minSimilarity: Int)(telegramFile: TelegramFile): List[IqdbResult] = {
       val response = http("https://iqdb.org/")
@@ -35,121 +35,117 @@ trait IqdbCommand extends Command with ExtractImage with Http {
           case s => s
         }
         matches = similarity >= minSimilarity
-      } yield IqdbResult(url, similarity, matches)).toList
+        booruService = BooruService.findByUrl(url)
+        if booruService.nonEmpty
+      } yield IqdbResult(url, booruService.get, similarity, matches)).toList
     }
 
-    case class ImageData(url: String, pageUrl: String, characters: Set[String], copyrights: Set[String],
-      artists: Set[String], image: Option[Array[Byte]] = None)
+    def applyPriority(priority: List[String])(iqdbResults: List[IqdbResult]): List[IqdbResult] = {
+      val servicesPriority = priority.flatMap(BooruService.findByName)
 
-    def readBooruPage(pageUrl: String): (ImageData, BooruService) = {
-      BooruService.findByUrl(pageUrl).map { booruService =>
-        val response = http(pageUrl, proxy = true).asString
-        if (response.code == 200) {
-          booruService.parseHtml(response.body) match {
-            case Some((url, characters, copyrights, artists)) =>
-              val imageData = ImageData(url, pageUrl, characters, copyrights, artists)
-              (imageData, booruService)
-            case None => throw new CommandException(s"Not parsed: $pageUrl.")
-          }
-        } else {
-          val code = response.code
-          throw new Exception(s"Invalid response: $code.")
-        }
-      }.getOrElse(throw new CommandException("Unknown service."))
-    }
-
-    def readBooruPages(priority: List[String])(iqdbResults: List[IqdbResult]): ImageData = {
-      case class BooruPage(imageData: Option[ImageData], service: BooruService, similarity: Int)
-      case class Result(list: List[BooruPage], exception: Option[Exception])
-
-      val result = {
-        val result = iqdbResults.foldLeft(Result(Nil, None)) { (result, iqdbResult) =>
-          if (iqdbResult.matches) {
-            try {
-              val (imageData, booruService) = readBooruPage(iqdbResult.url)
-              result.copy(list = BooruPage(Some(imageData), booruService, iqdbResult.similarity) :: result.list)
-            } catch {
-              case e: Exception => result.copy(exception = result.exception orElse Some(e))
-            }
-          } else {
-            BooruService.findByUrl(iqdbResult.url)
-              .map(s => result.copy(list = BooruPage(None, s, iqdbResult.similarity) :: result.list))
-              .getOrElse(result)
-          }
-        }
-
-        val servicesPriority = priority.flatMap(BooruService.findByName)
-
-        result.copy(list = result.list.sortWith { (a, b) =>
-          val aindex = servicesPriority.indexOf(a.service)
-          val bindex = servicesPriority.indexOf(b.service)
-          if (aindex >= 0 && bindex >= 0) {
-            if (aindex == bindex) {
-              a.similarity > b.similarity
-            } else {
-              aindex < bindex
-            }
-          } else if (aindex >= 0 || bindex >= 0) {
-            aindex >= 0
-          } else {
+      iqdbResults.sortWith { (a, b) =>
+        val aindex = servicesPriority.indexOf(a.booruService)
+        val bindex = servicesPriority.indexOf(b.booruService)
+        if (aindex >= 0 && bindex >= 0) {
+          if (aindex == bindex) {
             a.similarity > b.similarity
+          } else {
+            aindex < bindex
           }
-        })
-      }
-
-      result.list
-        .map(_.imageData).find(_.nonEmpty).flatten
-        .getOrElse {
-        if (result.list.nonEmpty) {
-          val notFoundMessage = result.exception.map { e =>
-            handleException(e)
-            "No images found (due to exception thrown)."
-          }.getOrElse("No images found.")
-
-          val (_, additionalResults) = result.list.map { booruPage =>
-            val serviceName = booruPage.service.commonNames.head
-            val similarity = booruPage.similarity
-            s"$similarity% — $serviceName"
-          }.foldLeft((1, "")) { (tuple, value) =>
-            val (count, result) = tuple
-            (count + 1, result + s"\n$count: $value")
-          }
-
-          throw new CommandException(s"$notFoundMessage\n\nAdditional results:$additionalResults")
+        } else if (aindex >= 0 || bindex >= 0) {
+          aindex >= 0
         } else {
-          throw result.exception.getOrElse(new CommandException("No images found."))
+          a.similarity > b.similarity
         }
       }
     }
 
-    def readBooruImage(imageData: ImageData): ImageData = {
-      val response = http(imageData.url, proxy = true).asBytes
+    case class ImageData(url: String, pageUrl: String,
+      characters: Set[String], copyrights: Set[String], artists: Set[String])
+
+    case class ReadImageData(name: String, image: Array[Byte], pageUrl: String,
+      characters: Set[String], copyrights: Set[String], artists: Set[String])
+
+    def readBooruPage(iqdbResult: IqdbResult): ImageData = {
+      val pageUrl = iqdbResult.url
+      val response = http(pageUrl, proxy = true).asString
       if (response.code == 200) {
-        imageData.copy(image = Some(response.body))
+        iqdbResult.booruService.parseHtml(response.body) match {
+          case Some((url, characters, copyrights, artists)) =>
+            ImageData(url, pageUrl, characters, copyrights, artists)
+          case None => throw new CommandException(s"Not parsed: $pageUrl.")
+        }
       } else {
         val code = response.code
         throw new Exception(s"Invalid response: $code.")
       }
     }
 
-    def replyWithImage(imageData: ImageData): Future[Message] = {
-      val name = Some(imageData.url).map { text =>
-        if (text == null || text == "") "image.jpeg" else text
-      }.map { text =>
-        val index = text.lastIndexOf('/')
-        if (index >= 0) text.substring(index + 1) else text
-      }.map { text =>
-        val index = text.indexOf('?')
-        if (index >= 0) text.substring(0, index) else text
-      }.get
+    def readBooruImage(imageData: ImageData): ReadImageData = {
+      val response = http(imageData.url, proxy = true).asBytes
+      if (response.code == 200) {
+        val name = {
+          val url = imageData.url
+          val start = url.lastIndexOf('/') + 1
+          val end = url.indexOf('?', start)
+          if (end >= start) url.substring(start, end) else url.substring(start)
+        }
 
+        ReadImageData(name, response.body, imageData.pageUrl,
+          imageData.characters, imageData.copyrights, imageData.artists)
+      } else {
+        val code = response.code
+        throw new Exception(s"Invalid response: $code.")
+      }
+    }
+
+    def readBooruImages(iqdbResults: List[IqdbResult]): ReadImageData = {
+      case class Result(successImageData: Option[ReadImageData] = None,
+        additionalIqdbResults: List[IqdbResult] = Nil, exception: Option[Exception] = None)
+
+      val result = iqdbResults.foldLeft(Result()) { (result, iqdbResult) =>
+        if (result.successImageData.isEmpty && iqdbResult.matches) {
+          try {
+            result.copy(successImageData = Some(readBooruImage(readBooruPage(iqdbResult))))
+          } catch {
+            case e: Exception => result.copy(exception = result.exception orElse Some(e))
+          }
+        } else {
+          result.copy(additionalIqdbResults = iqdbResult :: result.additionalIqdbResults)
+        }
+      }
+
+      result.successImageData.getOrElse {
+        val notFoundMessage = result.exception.map { e =>
+          handleException(e)
+          "No images found (due to exception thrown)."
+        }.getOrElse("No images found.")
+
+        val (_, additionalResults) = result.additionalIqdbResults.reverse.map { iqdbResult =>
+          val serviceName = iqdbResult.booruService.commonNames.head
+          val similarity = iqdbResult.similarity
+          s"$similarity% — $serviceName"
+        }.foldLeft((1, "")) { (tuple, value) =>
+          val (count, result) = tuple
+          (count + 1, result + s"\n$count: $value")
+        }
+
+        if (additionalResults.isEmpty) {
+          throw result.exception.getOrElse(new CommandException(s"$notFoundMessage"))
+        } else {
+          throw new CommandException(s"$notFoundMessage\n\nAdditional results:$additionalResults")
+        }
+      }
+    }
+
+    def replyWithImage(imageData: ReadImageData): Future[Message] = {
       def appendIterable(title: String, list: Iterable[String])(s: String): String = {
         if (list.nonEmpty) s + s"\n$title: " + list.reduceLeft(_ + ", " + _) else s
       }
 
       val captionOption = Some(imageData.pageUrl).map(appendIterable("Characters", imageData.characters))
         .map(appendIterable("Copyrights", imageData.copyrights)).map(appendIterable("Artists", imageData.artists))
-      request(SendDocument(Left(message.sender), Left(InputFile(name, imageData.image.get)),
+      request(SendDocument(Left(message.sender), Left(InputFile(imageData.name, imageData.image)),
         replyToMessageId = Some(message.messageId), caption = captionOption))
     }
 
@@ -167,9 +163,9 @@ trait IqdbCommand extends Command with ExtractImage with Http {
         val priority = arguments.string("p", "priority")
           .map(_.split(",|\\s+").toList.filter(!_.isEmpty)).getOrElse(Nil)
 
-        Future(obtainMessageFileId).flatMap(readTelegramFile).map(sendIqdbRequest(similarity))
-          .map(readBooruPages(priority)).map(readBooruImage).flatMap(replyWithImage)
-          .recoverWith(handleError("image request"))
+        Future(obtainMessageFileId).flatMap(readTelegramFile)
+          .map(sendIqdbRequest(similarity)).map(applyPriority(priority)).map(readBooruImages)
+          .flatMap(replyWithImage).recoverWith(handleError("image request"))
     }
   }
 }
